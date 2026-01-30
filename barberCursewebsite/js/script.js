5b5a5e4eb794efe947d3c96ef0b94edc7b45c83e
Tayssa Beatriz <tayssabeatriz40@gmail.com>
2026-01-30 14:29:42 -0300

    created img folder

--- barberCursewebsite/js/script.js
+++ barberCursewebsite/js/script.js
@@ -1,17 +1,50 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const comentarios = [
+    { nome: "Luan Victor", relato: "“Curso excelente! Aprendi do zero e hoje já consigo atender clientes com confiança. A didática é muito clara e prática.”", img: "img/userPic-1.jpg" },
+
+    { nome: "Luan Victor", relato: "“Superou minhas expectativas. O instrutor explica muito bem e passa várias dicas que realmente fazem diferença no dia a dia da barbearia.”", img: "img/userPic-2.jpg" },
+    { nome: "Lucas Silva", relato: "“Conteúdo completo, desde o básico até técnicas mais avançadas. Vale muito a pena para quem quer entrar na área ou se aperfeiçoar.”", img: "img/userPic-3.jpg" },
+    { nome: "waldir de Oliveira", relato: "“Curso muito bem organizado e direto ao ponto. Aprendi cortes modernos, acabamento perfeito e atendimento ao cliente.”", img: "img/userPic-4.jpg" },
+    { nome: "Paulo carneiro", relato: "Ótimo custo-benefício. As aulas práticas ajudam demais e dão segurança para começar a trabalhar logo após o curso!", img: "img/userPic-5.jpg" },
+    { nome: "Sandro roberto", relato: "Recomendo demais! Além da parte técnica, o curso ensina sobre postura profissional e como fidelizar clientes.", img: "img/usePic-6.jpg" },
+
+  ]
+  const commentsGrid = document.querySelector('.carrosselComments')
+
+  comentarios.forEach(comment => {
+
+    const commentsCard = document.createElement('div')
+    commentsCard.classList.add('cardComment')
+
+
+    commentsCard.innerHTML = `
+     <div class="containerStar">
+        <span>&#10029;</span>
+        <span>&#10029;</span>
+        <span>&#10029;</span>
+        <span>&#10029;</span>
+        <span>&#10029;</span>
+
+        </div>
+        <p>"${comment.relato}"</p>
+        <div class="containerUser">
+        <img src=${comment.img} alt="">
+        <div class="containerNome">
+        <h4>${comment.nome}</h4>
+        <p>concluiu o SUAT</p>
+        </div>
+        </div>
+    
+    `
+    commentsGrid.appendChild(commentsCard)
+
+
+  });
+
 let containerFaq = document.querySelector('.containerFaq');
-
-// function aoPassaroMouse(questFaq) {
-//   questFaq.style.backgroundColor = "#FACC2E";
-//   questFaq.style.color = "black"
-
-// }
-
-// function aoRetirarOmouse(questFaq) {
-//   questFaq.style.backgroundColor = '#2e2e38';
-//   questFaq.style.color = "white"
-
-// }
-
 function mostrarResposta(answerFaq, questFaq, textAnswer) {
   // alterna entre mostrar e esconder
 
@@ -24,8 +57,7 @@ function mostrarResposta(answerFaq, questFaq, textAnswer) {
   answerFaq.style.transition = 'height 0.4s ease'
 
 }
-
-
+  
 // Perguntas e respostas do FAQ
 const TextFaq = [
   { pergunta: 'O que eu vou receber?', resposta: 'Você receberá acesso completo ao curso e todos os materiais.' },
@@ -60,48 +92,6 @@ TextFaq.forEach(item => {
   containerFaq.appendChild(answerFaq);
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const comentarios = [
-    { nome: "Luan Victor", relato: "“Curso excelente! Aprendi do zero e hoje já consigo atender clientes com confiança. A didática é muito clara e prática.”", img: "../img/userPic-1.jpg" },
-
-    { nome: "Luan Victor", relato: "“Superou minhas expectativas. O instrutor explica muito bem e passa várias dicas que realmente fazem diferença no dia a dia da barbearia.”", img: "../img/userPic-2.jpg" },
-    { nome: "Lucas Silva", relato: "“Conteúdo completo, desde o básico até técnicas mais avançadas. Vale muito a pena para quem quer entrar na área ou se aperfeiçoar.”", img: "../img/userPic-3.jpg" },
-    { nome: "waldir de Oliveira", relato: "“Curso muito bem organizado e direto ao ponto. Aprendi cortes modernos, acabamento perfeito e atendimento ao cliente.”", img: "../img/userPic-4.jpg" },
-    { nome: "Paulo carneiro", relato: "Ótimo custo-benefício. As aulas práticas ajudam demais e dão segurança para começar a trabalhar logo após o curso!", img: "../img/userPic-5.jpg" },
-    { nome: "Sandro roberto", relato: "Recomendo demais! Além da parte técnica, o curso ensina sobre postura profissional e como fidelizar clientes.", img: "../img/usePic-6.jpg" },
-
-  ]
-  const commentsGrid = document.querySelector('.carrosselComments')
-
-  comentarios.forEach(comment => {
-
-    const commentsCard = document.createElement('div')
-    commentsCard.classList.add('cardComment')
-
-
-    commentsCard.innerHTML = `
-     <div class="containerStar">
-        <span>&#10029;</span>
-        <span>&#10029;</span>
-        <span>&#10029;</span>
-        <span>&#10029;</span>
-        <span>&#10029;</span>
-
-        </div>
-        <p>"${comment.relato}"</p>
-        <div class="containerUser">
-        <img src=${comment.img} alt="">
-        <div class="containerNome">
-        <h4>${comment.nome}</h4>
-        <p>concluiu o SUAT</p>
-        </div>
-        </div>
-    
-    `
-    commentsGrid.appendChild(commentsCard)
-
-
-  });
 
 
 
